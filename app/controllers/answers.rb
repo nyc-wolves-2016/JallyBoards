799a347f7e75b5ec119db2ba75da64ebeb09bd40
Jack Feely <jack.feely@gmail.com>
2016-09-21 19:15:40 -0400

    implement vote feature on questions and answers

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,12 +1,18 @@
-post '/questions/:id/answer' do
+post '/questions/:id' do
   answer = Answer.new(params[:answer])
-  question.user_id = current_user.id
+  answer.user_id = current_user.id
   answer.question_id = params[:id]
 
   if answer.save
-    redirect '/questions/#{params[:id]}'
+    redirect "/questions/#{params[:id]}"
   else
     @errors = answer.errors.full_messages
     erb :'questions/:id'
   end
+end
+
+get '/questions/:question_id/answers/:id/votes' do
+  answer = Answer.find(params[:id])
+  vote = Vote.create(user_id: current_user.id, voteable: answer)
+  redirect "/questions/#{params[:question_id]}"
 end
