3d9b5bbe368a5bcb3cbab765ef45ddccabd5b3c2
Thomas Graves <thomaslogangraves@gmail.com>
2016-09-21 15:25:20 -0400

    add in constraint

--- db/migrate/20160921110643_create_questions.rb
+++ db/migrate/20160921110643_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.string :question_title, null: false
-      t.string :question_body
+      t.string :question_body, null: false
       t.references :user, null: false
 
       t.timestamps
