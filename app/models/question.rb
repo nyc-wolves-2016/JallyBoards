b3921882b44da7d54ec939c889799ab973afd267
Angel Chen <rikugou@gmail.com>
2016-09-21 15:36:53 -0400

    Modify question model for readability. Change seed data ase needed

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,8 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :votes, as: :voteable
 
-  validates :question,
+  validates :title,
+            :content,
             :user,
               presence: true
 end
