59cb7f1e7887c019813ce76872867a936fa1be02
Angel Chen <rikugou@gmail.com>
2016-09-21 16:10:54 -0400

    Add error partial, display questions

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,8 +1,18 @@
+get'/questions' do
+  @questions = Question.all
+  erb :'/questions/index'
+end
+
 get '/questions/new' do
   erb :'/questions/new'
 end
 
 post '/questions' do
-
-  redirect '/questions/:id'
+  question = Question.new(params[:question])
+  if question.save
+    redirect '/questions'
+  else
+    @errors = question.errors.full_messages
+  end
+  erb :'/questions/new'
 end
