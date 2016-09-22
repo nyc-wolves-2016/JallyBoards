819914e41919bccd0e5378726a0410eaa86aff43
Thomas Graves <email": null>
2016-09-22 12:46:21 -0400

    Fix nameing for classes.

--- public/js/application.js
+++ public/js/application.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-  $('.voteForm').on('mouseover', '.upVoteButton', function(event){
-    $(this).closest('.voteForm').find('.loginBox').slideToggle();
+  $('.voteForm').on('mouseover', '.up-vote-button', function(event){
+    $(this).closest('.vote-form').find('.login-box').slideToggle();
   });
   function loginToggler(loginBox) {
-    var loginText = $('.loginBox').find('input[name="email"]').val();
+    var loginText = $('.login-box').find('input[name="email"]').val();
     if (!loginText) {
       $(loginBox).slideToggle();
     };
   };
-  $('.loginBox').on('mouseleave', function(event){
-    loginToggler($(event.target).closest('.loginBox'));
+  $('.login-box').on('mouseleave', function(event){
+    loginToggler($(event.target).closest('.login-box'));
   });
 });
