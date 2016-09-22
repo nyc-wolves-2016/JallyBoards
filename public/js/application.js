d59fe18483fac0f7f11085e84b4ae8bc9902f490
Thomas Graves <email": null>
2016-09-22 11:42:49 -0400

    Add event listener to toggle the login form for voting.

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.voteForm').on('mouseover', '.upVoteButton', function(event){
+    $(this).closest('.voteForm').find('.loginBox').slideToggle();
+  });
+  function loginToggler {
+    var loginText = $('.loginBox').find('input[name="email"]').val();
+    if (!loginText) {
+      $(this).slideToggle();
+    };
+  };
+  $('.loginBox').on('mouseleave', loginToggler {
+    // var loginText = $('.loginBox').find('input[name="email"]').val();
+    // if (!loginText) {
+    // $(this).slideToggle();
+  };
+  });
 });
